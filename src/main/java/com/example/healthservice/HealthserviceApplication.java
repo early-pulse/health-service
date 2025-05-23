d7313fb2429883e53435f3f23a2e17481a8e070d
Ashwinitiwari18 <97806314+Ashwinitiwari18@users.noreply.github.com>
2025-05-23 20:54:20 +0530

    first commit

---
+++ src/main/java/com/example/healthservice/HealthserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthserviceApplication.class, args);
+	}
+
+}
